b18232998289d15ae4d8c41a176de588ee42cc49
Guilherme Specht <gmspecht27@gmail.com>
2025-03-13 16:13:28 -0300

    Código do promblema dos triângulos

--- T1/triangulo.c
+++ T1/triangulo.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 
-const char* ClassificarTriangulo(int a, int b, int c) {
+const char* ClassificarTriangulo(int a, int b, int c) 
+{
     // Verifica se os valores estão dentro do intervalo permitido
-    if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200) {
+    if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200) 
+    {
         return "Valor fora do intervalo";
     }
     
     // Verifica se satisfazem a condição de existência de um triângulo
-    if (a >= b + c || b >= a + c || c >= a + b) {
+    if (a >= b + c || b >= a + c || c >= a + b) 
+    {
         return "Nao e um Triângulo";
     }
     
     // Classificação do triângulo
-    if (a == b && b == c) {
+    if (a == b && b == c) 
+    {
         return "Equilatero";
-    } else if (a == b || a == c || b == c) {
+    } 
+    else if (a == b || a == c || b == c) 
+    {
         return "Isosceles";
-    } else {
+    } 
+    else 
+    {
         return "Escaleno";
     }
 }
 
-int main() {
+int main() 
+{
     int a, b, c;
     printf("Digite os tres lados do triangulo: ");
     scanf("%d %d %d", &a, &b, &c);
